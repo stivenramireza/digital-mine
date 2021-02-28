077abfe8648181098861087a8b1f6c674f4f51f2
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-02-28 16:54:21 -0500

    CartList component

--- pages/carrito.vue
+++ pages/carrito.vue
@@ -9,56 +9,7 @@
           </template>
         </v-expansion-panel-header>
         <v-expansion-panel-content color="blue-grey darken-3 white--text">
-          <v-list color="blue-grey darken-3">
-            <v-list-item>
-              <v-list-item-avatar></v-list-item-avatar>
-              <v-list-item-title class="product"
-                ><strong>Producto</strong></v-list-item-title
-              >
-              <v-list-item-title><strong>Cantidad</strong></v-list-item-title>
-              <v-list-item-title><strong>Precio</strong></v-list-item-title>
-            </v-list-item>
-            <v-list-item
-              v-for="(item, index) in products"
-              :key="index"
-              color="white"
-            >
-              <v-list-item-avatar>
-                <v-img :src="item.imageUrl"></v-img>
-              </v-list-item-avatar>
-              <v-list-item-title class="product">
-                {{ item.name }}</v-list-item-title
-              >
-              <v-list-item-title v-if="[1, 6].includes(item.id)">
-                <v-select
-                  :value="item.quantity"
-                  background-color="blue-grey darken-3"
-                  :items="screens"
-                  color="white"
-                  item-color="blue-grey darken-3"
-                  dark
-                  @change="updateQuantity({ quantity: $event, id: item.id })"
-                >
-                </v-select>
-              </v-list-item-title>
-              <v-list-item-title v-else>{{ item.quantity }}</v-list-item-title>
-              <v-list-item-title>{{
-                (item.price * item.quantity) | priceFormat
-              }}</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-avatar color="white--text"
-                ><strong>Total</strong></v-list-item-avatar
-              >
-              <v-list-item-title class="product"></v-list-item-title>
-              <v-list-item-title></v-list-item-title>
-              <v-list-item-title
-                ><strong>{{
-                  totalPrice | priceFormat
-                }}</strong></v-list-item-title
-              >
-            </v-list-item>
-          </v-list>
+          <CartList />
         </v-expansion-panel-content>
       </v-expansion-panel>
 
@@ -87,32 +38,16 @@
 </template>
 
 <script>
-import { mapState, mapMutations, mapGetters } from 'vuex'
-const numeral = require('numeral')
+import CartList from '@/components/common/CartList'
+
 export default {
-  filters: {
-    priceFormat: (value) => {
-      return '$' + numeral(value).format('0,0,0,0,0')
-    },
+  components: {
+    CartList,
   },
   data() {
     return {
       panel: 0,
-      screens: [1, 2, 3, 4, 5],
     }
-  },
-  computed: {
-    ...mapState({
-      products: (state) => state.cart.products,
-    }),
-    ...mapGetters({
-      totalPrice: 'cart/totalPrice',
-    }),
-  },
-  methods: {
-    ...mapMutations({
-      updateQuantity: 'cart/updateQuantity',
-    }),
   },
 }
 </script>
@@ -146,11 +81,6 @@ export default {
 @media (min-width: 320px) and (max-width: 767px) {
   .expansion-panels-container {
     margin: 20px;
-  }
-}
-@media (min-width: 320px) and (max-width: 592px) {
-  .product {
-    display: none;
   }
 }
 </style>
